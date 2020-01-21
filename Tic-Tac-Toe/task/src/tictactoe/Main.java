7ba84787ebd0d93cc6abbc17e78d72c198a6ca61
s3rgh <s3rgh@mail.ru>
2020-01-22 00:46:14 +0300

    tictactoe 4/5 22.01.2020

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,20 +1,67 @@
 package tictactoe;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         // write your code here
+        System.out.print("Enter cells: ");
         Scanner scanner = new Scanner(System.in);
-        char[] arr = scanner.next().toCharArray();
+        char[] arr = scanner.next().toCharArray(); // array of full cells
+        char[][] tictac = new char[3][3];          // M x M array for my grid
+        int k = 0;
+        int coordI;
+        int coordJ;
+
+        // filling of grid
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tictac[i][j] = arr[k];
+                k++;
+            }
+        }
+
+
+        printGrid(tictac);
+        tictac = turnToRight(tictac);
+
+        while (true) {
+            System.out.print("Enter the coordinates: ");
+
+            try {
+                coordI = scanner.nextInt();
+                coordJ = scanner.nextInt();
+                //System.out.println();
+
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!"); // if exception, try again
+                scanner.nextLine();
+                continue;
+            }
+
+            if ((coordI < 1 || coordI > 3) || (coordJ < 1 || coordJ > 3)) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                continue;
+            } else if (checkCell(coordI, coordJ, tictac)) {
+                System.out.println("This cell is occupied! Choose another one!");
+                continue;
+            } else {
+                tictac[coordI - 1][coordJ - 1] = 'X';
+                printGridAndRotate(tictac);
+                /*if (checkGrid(tictac)){
+                    break;
+                }*/
+                break;
+            }
+        }
+
+        /*
+
         int countX = 0;
         int countO = 0;
         int countEmpty = 0;
-        System.out.println("---------");
-        System.out.println("| " + arr[0] + " " + arr[1] + " " + arr[2] + " |");
-        System.out.println("| " + arr[3] + " " + arr[4] + " " + arr[5] + " |");
-        System.out.println("| " + arr[6] + " " + arr[7] + " " + arr[8] + " |");
-        System.out.println("---------");
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 'X') {
@@ -25,6 +72,7 @@ public class Main {
                 countEmpty++;
             }
         }
+
         boolean firstLineX = arr[0] == 'X' && arr[1] == 'X' && arr[2] == 'X';
         boolean secondLineO = arr[3] == 'O' && arr[4] == 'O' && arr[5] == 'O';
         boolean secondLineX = arr[3] == 'X' && arr[4] == 'X' && arr[5] == 'X';
@@ -73,7 +121,6 @@ public class Main {
                 || secondDiagX
                 || secondDiagO);
 
-
         if (impossible) {
             System.out.println("Impossible");
         } else if (notFinished && countEmpty != 0) {
@@ -84,7 +131,101 @@ public class Main {
             System.out.println("O wins");
         } else if (countEmpty == 0) {
             System.out.println("Draw");
+        }*/
+
+    }
+
+    public static boolean checkCell(int i, int j, char[][] chars) {
+        if (chars[i - 1][j - 1] != '_') {
+            return true;
+        } else {
+            return false;
         }
+    }
+
+    public static boolean checkGrid(char[][] chars) {
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < chars[i].length; j++) {
+                if (chars[i][j] == '_') {
+                    count++;
+                }
+            }
+        }
+        if (count == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Method can print my grid
+     * and all cells
+     *
+     * @param arr
+     */
+    public static void printGrid(char[][] arr) {
+
+        System.out.println("---------");
+        System.out.println("| " + arr[0][0] + " " + arr[0][1] + " " + arr[0][2] + " |");
+        System.out.println("| " + arr[1][0] + " " + arr[1][1] + " " + arr[1][2] + " |");
+        System.out.println("| " + arr[2][0] + " " + arr[2][1] + " " + arr[2][2] + " |");
+        System.out.println("---------");
+    }
+
+    /**
+     * Method rotates my matrix
+     *
+     * @param arr1
+     * @name tictac
+     * Also it prints my grid and all cells
+     */
+    public static void printGridAndRotate(char[][] arr1) {
+
+        char[][] arr = turnToLeft(arr1);
+
+        System.out.println("---------");
+        System.out.println("| " + arr[0][0] + " " + arr[0][1] + " " + arr[0][2] + " |");
+        System.out.println("| " + arr[1][0] + " " + arr[1][1] + " " + arr[1][2] + " |");
+        System.out.println("| " + arr[2][0] + " " + arr[2][1] + " " + arr[2][2] + " |");
+        System.out.println("---------");
+    }
+
+    /**
+     * Method rotates matrix to the left
+     *
+     * @param arr
+     * @return result[][]
+     */
+    public static char[][] turnToLeft(char[][] arr) {
+        int m = arr.length;
+        char[][] result = new char[m][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                result[m - j - 1][i] = arr[i][j];
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Method rotates matrix to the right
+     *
+     * @param arr
+     * @return result[][]
+     */
+    public static char[][] turnToRight(char[][] arr) {
+        int m = arr.length;
+        char[][] result = new char[m][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                result[j][m - i - 1] = arr[i][j];
+            }
+        }
+        return result;
     }
 }
 
